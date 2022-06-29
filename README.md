b9124f307906ca4bfeab6ae7c670c68a1d91ce60
tincturetinkerer <tincturetinkerer@gmail.com>
2022-06-29 19:51:12 -0400

    Add data management

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tincturetinkerer.github.io
 Tincture Tinkerer web app
+
+To push to the live site, use: git subtree push --prefix dist origin gh-pages
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,13 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },{
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
       },
     ],
   },
